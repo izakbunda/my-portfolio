3123e264642bceb69219d134a0b3482f50dc9b23
Izak Bunda <izakbunda@gmail.com>
2024-09-18 22:36:53 -0700

    update profile

--- my-portfolio/src/components/Profile/Profile.jsx
+++ my-portfolio/src/components/Profile/Profile.jsx
@@ -3,11 +3,13 @@ import "./Profile.css";
 
 const Profile = () => {
   const bio1 =
-    "I'm a junior at the University of California Los Angeles majoring in Computer Science! I'm interested in delivering high-impact and efficient software projects and creatively solving practical problems using tech! 🤩👨‍💻";
+    "I'm a senior at the University of California Los Angeles majoring in Computer Science! I'm interested in delivering high-impact and efficient software projects and creatively solving practical problems using tech! 🤩👨‍💻";
   const bio2 =
     "This past summer, I worked at Hunter Industries as a Software Development Intern, completed summer courses, and worked on personal projects on the side. I've previously worked at Retune (a DevX project), FanSpace (a Creative Labs project), and a start-up, Hussle. I'm an experienced mobile and web developer skilled in React, React Native, Javascript, C++, Python, object oriented programming, data structures, version control (git), and project management. And I'm interested in learning more about operating systems, embedded software, artificial intelligence, and machine learning.";
   const bio3 =
     "If I'm not coding on my computer, you can probably find me reading a book, at the gym, or cooking. 💪";
+  const bio4 =
+    "Actively searching for internships and full time new-grad roles! Thanks for visiting my site :-)";
 
   const tags = ["frontend", "backend", "database", "machine learning"];
 
@@ -50,8 +52,9 @@ const Profile = () => {
             </a> */}
           </div>
           <p>{bio1}</p>
-          <p>{bio2}</p>
+          {/* <p>{bio2}</p> */}
           <p>{bio3}</p>
+          <p>{bio4}</p>
         </div>
         {/* <div className="profile-section">
           <h3>Work history</h3>
