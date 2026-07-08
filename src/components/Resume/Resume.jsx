1139dcc38c7c2bced9fd52254d0c42a2144a5f06
Izak Bunda <izakbunda@Izaks-MacBook-Pro.local>
2026-07-08 15:09:54 -0600

    resume update

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
     <div className="resume-container">
       <div className="download-link-container">
         <a
-          href="https://drive.google.com/file/d/110NCCYUz4tEqggGPKPVwG2oogLQVeb97/view?usp=sharing"
+          href="https://drive.google.com/file/d/1mo-dBuWk_-9XBq3mPkFX6mn0h2dX0AOj/view?usp=sharing"
           className="download-link"
           target="_blank"
           rel="noopener noreferrer"
@@ -16,7 +16,7 @@ const Resume = () => {
       </div>
 
       <iframe
-        src="https://drive.google.com/file/d/110NCCYUz4tEqggGPKPVwG2oogLQVeb97/preview"
+        src="https://drive.google.com/file/d/1mo-dBuWk_-9XBq3mPkFX6mn0h2dX0AOj/preview"
         className="resume-iframe"
         title="Resume"
       />
